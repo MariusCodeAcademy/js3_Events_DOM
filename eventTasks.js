6d2af877adb519da71790d688d057c854ae26531
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-05-03 15:01:13 +0300

    eventTasks

--- eventTasks.js
+++ eventTasks.js
@@ -17,3 +17,21 @@
 // 5 sukurti nav, kurioje butu 3-5 nuorodos
 // sukurti mygtuka nuorodu desineje ir kad paspaudus mygtuka
 // nuorodos pasisleptu ir liktu tik burger mygtukas kuri paspaudus vel matytit nuorodas
+
+// 6 sukurti ivesties lauka htmle, salia mygtuka
+// 6.1 padaryti kad ivedant teksta i lauka tekstas atsivaizduotu mygtuke
+// 6.2 padaryti kad yvendant tekstas atsivaizduotu H1 elemente documento virsuje
+// 6.3 padaryti kad ivedus spalva angliskai i ivesties lauka dokumento body
+// taptu tokios spalvos
+// 6.4 padaryti kad paspaudus mygtuka ivestas tekstas butu atvaizduotas is priesingos puses
+
+// 7. prideti 4tu punktu sukurtam sarasui eventlistenerius kad paspaudus ta el jam
+// uzsidetu klase .active . aprasyti klase kad el isiskirtu vizualiai is kitu
+
+// 8 sukurti html dar viena mygtuka su ivesties lauku ir divu 100px aukscio
+// ivesti i lauka keturias spalvas angiskai atskirtas tarpu
+// paspaudus mygtuka turetu keistis divo fono spalva is eiles pagal ivestas spalvas.
+
+/// 9 dinamiskai js sugeneruoti modala ir atvaizduoti ji paspausus html sukurta
+// mygtuka. Modale yra antraste paragrafas ir mygtukas
+// 9.1 Modale turetu buti mygtukas close, kuri pasalina sukurta modala is DOM
