93bae6a098f6bab6f693195e33514cc802d9cd35
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-05-03 15:03:01 +0300

    eventTasks

--- eventTasks.js
+++ eventTasks.js
@@ -5,16 +5,16 @@
 // susikurti mygtukus:
 
 // 2. mygdukas kuris padaro body spalva juoda ir texta balta
-// 3. mygtukas po kuriuo sukuriam div. padarom diva 100px
+// 3. [html] mygtukas po kuriuo sukuriam div. padarom diva 100px
 // x 100px su apvadu
-// padadom kad paspaudus mygtuka pasikeistu div spalva
+// [js]padadom kad paspaudus mygtuka pasikeistu div spalva
 
 // 4. po treciu mygtuku sugeneruoti rikiuota sarasa su texktu
 // item_1, item_2, item_3
 // sugeneruoti 3 itemus kai tik uzkraunam puslapi
 // generuoti papildoma itema kai paspaudziam mygtuka
 
-// 5 sukurti nav, kurioje butu 3-5 nuorodos
+// 5 [html] sukurti nav, [js] kurioje butu 3-5 nuorodos
 // sukurti mygtuka nuorodu desineje ir kad paspaudus mygtuka
 // nuorodos pasisleptu ir liktu tik burger mygtukas kuri paspaudus vel matytit nuorodas
 
